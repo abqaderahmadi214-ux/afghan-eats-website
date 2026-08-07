03c4e80cd11cbe2093a56be13ec0ad1624266f7f
abqaderahmadi214-ux <ab.qaderahmadi214@gmail.com>
2026-08-07 17:41:18 +0430

    Configure production Afghan Eats API

---
+++ config.js
@@ -0,0 +1,6 @@
+window.AFGHAN_EATS_CONFIG = {
+  apiBaseUrl: 'https://afghaneats-api.onrender.com',
+  supportPhone: '+93 796 851 968',
+  supportEmail: 'support@afghaneats.net',
+  launchCity: 'Herat'
+};
