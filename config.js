0ec6bb333f87f67951a42c3dfc952d8b6b0d4437
abqaderahmadi214-ux <ab.qaderahmadi214@gmail.com>
2026-08-28 16:45:37 +0000

    Prepare official customer app store links

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ window.AFGHAN_EATS_CONFIG = {
   apiBaseUrl: 'https://afghaneats-api.onrender.com',
   supportPhone: '+93 796 851 968',
   supportEmail: 'support@afghaneats.net',
+  customerAndroidPackage: 'com.app.afghaneatsmobile',
+  customerAndroidUrl: '',
+  customerIosUrl: '',
   launchCity: 'Herat'
 };
 
