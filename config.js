599f004448c41fa65c5733694388eda0ba665ef7
abqaderahmadi214-ux <ab.qaderahmadi214@gmail.com>
2026-09-19 01:18:06 +0430

    Align website with Customer Android build 8

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.AFGHAN_EATS_CONFIG = {
   supportEmail: 'support@afghaneats.net',
   customerAndroidPackage: 'net.afghaneats.customer',
   customerAndroidVersion: '1.2.0',
-  customerAndroidVersionCode: 6,
+  customerAndroidVersionCode: 8,
   riderAndroidPackage: 'net.afghaneats.rider',
   riderAndroidVersion: '1.1.2',
   riderAndroidVersionCode: 4,
